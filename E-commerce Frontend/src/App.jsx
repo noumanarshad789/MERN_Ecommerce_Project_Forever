d6e95735d4da71e218d9e03392aa1d543649a095
unknown <mmnoumanarshad456@gmail.com>
2025-07-12 16:44:02 -0700

    Creating a new project and setting up the simple things of the project. This is the basic code and till now I am setup the Half of the navbar.

---
+++ E-commerce Frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Cart from './pages/Cart'
+import Collection from './pages/Collection'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import Order from './pages/Order'
+import Placeorder from './pages/Placeorder'
+import Products from './pages/Products'
+import Navbar from './components/Navbar'
+
+const App = () => {
+  return (
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/about' element={<About/>}></Route>
+        <Route path='/cart' element={<Cart/>}></Route>
+        <Route path='/collection' element={<Collection/>}></Route>
+        <Route path='/contact' element={<Contact/>}></Route>
+        <Route path='/login' element={<Login/>}></Route>
+        <Route path='/order' element={<Order/>}></Route>
+        <Route path='/placeorder' element={<Placeorder/>}></Route>
+        <Route path='/product' element={<Products/>}></Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
