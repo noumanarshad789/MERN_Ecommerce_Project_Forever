4cbb7610d881b700bb6a428f2dee153b9911a9aa
unknown <mmnoumanarshad456@gmail.com>
2025-07-14 04:05:40 -0700

    Till now I am adding the product page.

--- E-commerce Frontend/src/App.jsx
+++ E-commerce Frontend/src/App.jsx
@@ -8,10 +8,10 @@ import Contact from './pages/Contact'
 import Login from './pages/Login'
 import Order from './pages/Order'
 import Placeorder from './pages/Placeorder'
-import Products from './pages/Products'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import SearchBar from './components/SearchBar'
+import Product from './pages/Product'
 
 const App = () => {
   return (
@@ -27,7 +27,7 @@ const App = () => {
         <Route path='/login' element={<Login />}></Route>
         <Route path='/order' element={<Order />}></Route>
         <Route path='/placeorder' element={<Placeorder />}></Route>
-        <Route path='/product' element={<Products />}></Route>
+        <Route path='/product/:productId' element={<Product />}></Route>
       </Routes>
       <Footer />
     </div>
