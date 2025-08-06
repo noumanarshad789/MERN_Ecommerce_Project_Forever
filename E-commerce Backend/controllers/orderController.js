97d5de2632b2087218433f35334d691a4bdc417f
unknown <mmnoumanarshad456@gmail.com>
2025-08-06 07:31:14 +0500

    Setup the stripe payment gateway.

--- E-commerce Backend/controllers/orderController.js
+++ E-commerce Backend/controllers/orderController.js
@@ -1,5 +1,14 @@
 import orderModel from "../models/orderModel.js"
 import userModel from "../models/userModel.js"
+import Stripe from "stripe"
+
+// Global Variables
+const currency = "inr"
+const deliveryCharge = 100
+
+
+// Gateway Initializing
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 
 // Placing order using COD Method  
@@ -30,8 +39,6 @@ const placeOrder = async (req, res) => {
         console.log(error)
         res.json({ success: false, message: error.message })
     }
-
-
 }
 
 
@@ -39,7 +46,56 @@ const placeOrder = async (req, res) => {
 // Placing order using Stripe Method  
 
 const placeOrderStripe = async (req, res) => {
+    try {
+        const { userId, items, amount, address } = req.body
+        const { origin } = req.headers;
+        const orderData = {
+            userId,
+            items,
+            address,
+            amount,
+            paymentMethod: "Stripe",
+            payment: false,
+            date: Date.now()
+        }
+        const newOrder = new orderModel(orderData)
+        await newOrder.save()
 
+        const line_items = items.map((item) => ({
+            price_data: {
+                currency: currency,
+                product_data: {
+                    name: item.name
+                },
+                unit_amount: item.price * 100
+            },
+            quantity: item.quantity
+        }))
+
+        line_items.push({
+            price_data: {
+                currency: currency,
+                product_data: {
+                    name: "Delivery Charges"
+                },
+                unit_amount: deliveryCharge * 100
+            },
+            quantity: 1
+        })
+
+
+        const session = await stripe.checkout.sessions.create({
+            success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+            cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+            line_items,
+            mode: "payment",
+        })
+
+        res.json({ success: true, session_url: session.url })
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
 }
 
 
@@ -85,12 +141,12 @@ const userOrders = async (req, res) => {
 
 const updateStaus = async (req, res) => {
     try {
-        const {orderId, status} = req.body
-        await orderModel.findByIdAndUpdate(orderId,{status})
-        res.json({success:true, message:"Status Updated"})
+        const { orderId, status } = req.body
+        await orderModel.findByIdAndUpdate(orderId, { status })
+        res.json({ success: true, message: "Status Updated" })
     } catch (error) {
         console.log(error)
-        res.json({success:false, message:error.message})
+        res.json({ success: false, message: error.message })
 
     }
 }
